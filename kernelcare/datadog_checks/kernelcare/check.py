3a87562e8269419615f4b51348b183ae2e187781
Serhii Chvaliuk <schvaliuk@cloudlinux.com>
2020-07-10 05:48:31 +0000

    check.py added

--- kernelcare/datadog_checks/kernelcare/check.py
+++ kernelcare/datadog_checks/kernelcare/check.py
@@ -1,9 +1,38 @@
-from typing import Any
 
-from datadog_checks.base import AgentCheck
+import requests
+from datadog_checks.base import AgentCheck, ConfigurationError
 
 
 class KernelcareCheck(AgentCheck):
-    def check(self, _):
-        # type: (Any) -> None
-        pass
+
+    KEY_KCARE_NAGIOS_ENDPOINT = 'https://cln.cloudlinux.com/clweb/api/kcare/nagios/'
+
+    def _parse_nagios_response(self, text):
+
+        lines = text.split('\n')
+        _, params = lines[0].split('|', 1)
+
+        res = {}
+        for p in params.split(';'):
+            k, v = p.split('=')
+            res[k] = int(v)
+        return res
+
+    def check(self, instance):
+
+        key = instance.get('key')
+
+        if not key:
+            raise ConfigurationError('Configuration error, you must provide `key`')
+
+        url = self.KEY_KCARE_NAGIOS_ENDPOINT + key
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = self._parse_nagios_response(response.text)
+
+        self.gauge('kernelcare.uptodate', data['uptodate'])
+        self.gauge('kernelcare.outofdate', data['outofdate'])
+        self.gauge('kernelcare.unsupported', data['unsupported'])
+        self.gauge('kernelcare.inactive', data['inactive'])
