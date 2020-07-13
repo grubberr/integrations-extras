189601156fd39c1578fe0733d98d10e91cf90df1
Serhii Chvaliuk <schvaliuk@cloudlinux.com>
2020-07-13 17:45:33 +0000

    Reseller logins supported now

--- kernelcare/datadog_checks/kernelcare/check.py
+++ kernelcare/datadog_checks/kernelcare/check.py
@@ -7,11 +7,22 @@ from datadog_checks.base.errors import CheckException
 class KernelcareCheck(AgentCheck):
 
     KEY_KCARE_NAGIOS_ENDPOINT = 'https://cln.cloudlinux.com/clweb/api/kcare/nagios/'
+    RES_KCARE_NAGIOS_ENDPOINT = 'https://cln.cloudlinux.com/clweb/api/kcare/nagios-res/'
+
+    HTTP_RESPONSE_ERRORS = [
+        'Servers not found for key',
+        'Reseller not found',
+        'Registration token not found for reseller',
+    ]
 
     def _parse_nagios_response(self, text):
 
         lines = text.split('\n')
-        _, params = lines[0].split('|', 1)
+        tmp = lines[0].split('|', 1)
+        if len(tmp) == 1:
+            params = tmp[0]
+        else:
+            params = tmp[1]
 
         res = {}
         for p in params.split(';'):
@@ -19,20 +30,31 @@ class KernelcareCheck(AgentCheck):
             res[k] = int(v)
         return res
 
-    def check(self, instance):
+    def get_url(self, instance):
 
         key = instance.get('key')
 
-        if not key:
-            raise ConfigurationError('Configuration error, you must provide `key`')
+        if key:
+            return self.KEY_KCARE_NAGIOS_ENDPOINT + key
 
-        url = self.KEY_KCARE_NAGIOS_ENDPOINT + key
+        login = instance.get('login')
+        api_token = instance.get('api_token')
+
+        if login and api_token:
+            return self.RES_KCARE_NAGIOS_ENDPOINT + login + '/' + api_token
+
+        raise ConfigurationError('Configuration error, you must provide `key` or `login`')
+
+    def check(self, instance):
+
+        url = self.get_url(instance)
 
         response = requests.get(url)
         response.raise_for_status()
 
-        if response.text.startswith('Servers not found for key'):
-            raise CheckException(response.text)
+        for prefix in self.HTTP_RESPONSE_ERRORS:
+            if response.text.startswith(prefix):
+                raise CheckException(response.text)
 
         try:
             data = self._parse_nagios_response(response.text)
